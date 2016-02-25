17dd23d9c0cade61fb040863995c39a6a88cd7c5
mpharmon <mpharmon@email.arizona.edu>
2016-02-25 14:05:43 -0700

    Incremental Commit

--- main.c
+++ main.c
@@ -11,26 +11,25 @@ typedef enum enumState{
 }STATE;
 
 volatile STATE state = ENTER;
-volatile char lastCharacter = NULL;
 
 void main(){
-  int LCD_Line = 1;
-  int LCD_Character = NULL;
+  unsigned LCD_Line = 1;// Used to track what line the cursor is on
+  unsigned LCD_Character = 1;// Used to track what character in the line the cursor is on
   LCD_Init();
   KeyPad_Init();
   while(1){
     switch(state){
       case ENTER:
-        if(LCD_Character == 16){
-          LCD_Character = 1;
-          if(LCD_Line = 1)LCD_Line = 2;
-          else LCD_Line = 1;
-          LCD_MoveCursor(LCD_Character,LCD_Line);
+        if(LCD_Character == 16){// If we are at the end of the line
+          LCD_Character = 1;//Reset Character back to the beginning
+          if(LCD_Line = 1)LCD_Line = 2;//If we are on line 1 go to line 2
+          else LCD_Line = 1;//if we are on line 2 go to line 1
+          LCD_MoveCursor(LCD_Character,LCD_Line);// Print Character on Screen
         }else{
-          LCD_PrintChar(KeyPad_Scan());
-          LCD_Character++;
+          LCD_PrintChar(KeyPad_Scan());// Print Character on Screen
+          LCD_Character++;// Advance Character Counter
         }
-        state = WAIT;
+        state = WAIT;//Set state back to waiting
         break;
       case WAIT:
         break;
@@ -41,5 +40,5 @@ void main(){
 void __ISR(_CHANGE_NOTICE_VECTOR,IPL7SRS) _CNInterrupt{
   PORTx;
   IFS1bits.CNxIF = 0;// Put Flag Down
-  if()
+  state = ENTER;
 }
